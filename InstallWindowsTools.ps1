40a9eea1ded562dd2eddc9a91a6c93778a5bcbb8
John Moore <johnmmr@gmail.com>
2022-11-28 16:05:29 -0800

    Update InstallWindowsTools.ps1

--- InstallWindowsTools.ps1
+++ InstallWindowsTools.ps1
@@ -5,63 +5,63 @@
 
 Write-Output "Installing Apps"
 $apps = @(
-	# Windows Core Tools
+    # Windows Core Tools
     @{name = "Microsoft.dotnet" },
-	@{name = "Microsoft.DotNet.SDK.6" },
+    @{name = "Microsoft.DotNet.SDK.6" },
     @{name = "Microsoft.PowerShell" },
     @{name = "Microsoft.PowerToys" },
     @{name = "JanDeDobbeleer.OhMyPosh" },
     @{name = "Microsoft.WindowsTerminal" },
 
-	# Misc Utils
+    # Misc Utils
     @{name = "7zip.7zip" },
     @{name = "Adobe.Acrobat.Reader.64-bit" },
     @{name = "GnuPG.Gpg4win" },
-    @{name = "Greenshot.Greenshot" },	
+    @{name = "Greenshot.Greenshot" },    
     @{name = "NickeManarin.ScreenToGif" },
     @{name = "Notepad++.Notepad++" },
     @{name = "VideoLAN.VLC" },
     @{name = "WinDirStat.WinDirStat" },
-	@{name = "StefansTools.grepWin" },
+    @{name = "StefansTools.grepWin" },
     @{name = "Zoom.Zoom" },
-	@{name = "Microsoft.Teams" },
+    @{name = "Microsoft.Teams" },
     @{name = "Dropbox.Dropbox" },
-	@{name = "Microsoft.OneDrive" },
-	@{name = "Box.Box" },
-	
-	# Network/Internet Tools
-	@{name = "PuTTY.PuTTY" },
-	@{name = "mRemoteNG.mRemoteNG" },
+    @{name = "Microsoft.OneDrive" },
+    @{name = "Box.Box" },
+    
+    # Network/Internet Tools
+    @{name = "PuTTY.PuTTY" },
+    @{name = "mRemoteNG.mRemoteNG" },
     # @{name = "Devolutions.RemoteDesktopManagerFree" },
     @{name = "TimKosse.FileZilla.Client" },
-	
-	# Browsers
+    
+    # Browsers
     @{name = "Google.Chrome" },
     @{name = "Mozilla.Firefox.DeveloperEdition" },
     @{name = "Mozilla.Firefox" },
-	@{name = "Brave.Brave" },
+    @{name = "Brave.Brave" },
 
-	# Office
-	@{name = "TheDocumentFoundation.LibreOffice" },
-	@{name = "TheDocumentFoundation.LibreOffice.HelpPack" },
+    # Office
+    @{name = "TheDocumentFoundation.LibreOffice" },
+    @{name = "TheDocumentFoundation.LibreOffice.HelpPack" },
     @{name = "Inkscape.Inkscape" },
     @{name = "JohnMacFarlane.Pandoc" },
-	@{name = "GIMP.GIMP" },
-	@{name = "OpenShot.OpenShot" },
-	@{name = "Sigil-Ebook.Sigil" },
+    @{name = "GIMP.GIMP" },
+    @{name = "OpenShot.OpenShot" },
+    @{name = "Sigil-Ebook.Sigil" },
 
-	# Dev Tools
+    # Dev Tools
     @{name = "Git.Git" },
     @{name = "Axosoft.GitKraken" },
-	@{name = "TortoiseGit.TortoiseGit" },
+    @{name = "TortoiseGit.TortoiseGit" },
     @{name = "KDE.KDiff3" },
     @{name = "Microsoft.VisualStudio.2022.Community" },
     @{name = "Microsoft.VisualStudioCode" },
     @{name = "JetBrains.Toolbox" },
     @{name = "OpenJS.NodeJS.LTS" },
-	@(name = "AdoptOpenJDK.OpenJDK.16" },
-	@{name = "LINQPad.LINQPad.5" },
-	@{name = "LINQPad.LINQPad.6" },
+    @(name = "AdoptOpenJDK.OpenJDK.16" },
+    @{name = "LINQPad.LINQPad.5" },
+    @{name = "LINQPad.LINQPad.6" },
     # @{name = "Microsoft.AzureDataStudio" }, # Included with SSMS
     @{name = "Microsoft.SQLServerManagementStudio" }, # Includes AzureDataStudio
 );
